462cde5f0e50be164a1632428bd454707894d8bc
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-24 16:54:15 +0530

    Modified doubles.java

---
+++ Doubles/doubles.java
@@ -0,0 +1,19 @@
+package Doubles;
+
+public class doubles {
+    public static void main(String[] args){
+        double[] price = new double[2];
+        price[0] = 199.90;
+        price[1] = 200.45;
+        price[2] = 100.01;
+        price[3] = 1000.99;
+
+        for(int d = 2; d < price.length; d++){
+                System.out.println("Price = " + price[d]);
+        }
+
+
+
+    }
+    
+}
