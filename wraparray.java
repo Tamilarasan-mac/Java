2b1c7511a1a3e7de735c6066c57eb0e0cccfcfdd
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-30 10:27:40 +0530

    wraparray.java file added

---
+++ wraparray.java
@@ -0,0 +1,12 @@
+class wraparray {
+    public static void main(String[] args) {
+	
+	Integer[] numbers ={1, 2, 100, 1000};
+
+for(Integer n : numbers ) {
+
+            System.out.println("code :" +n);
+
+        }
+    }
+}
