82dbe241b66e82b10e1b4aef8eebdcbfe59b5f97
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-30 10:36:09 +0530

    max.java file added

---
+++ max.java
@@ -0,0 +1,16 @@
+public class max {
+	public static void main(String[] args) {
+		int[] values ={1005, 1001, 1002, 1003, 1004};
+
+		int max =values[1];
+
+	for (int i=0; i < values.length; i++) {
+		if (values[i] > max) {
+		max = values[i];
+			
+				}
+				
+										}
+		System.out.println("Max Num: "+ max);								
+										};									
+				}
