5475e2e657848cac501062396cc3f150f9782f8b
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-30 10:23:46 +0530

    Modified wrapper filr

---
+++ wrapper.java
@@ -0,0 +1,15 @@
+public class wrapper{
+
+	public static void main(String[] args){
+
+		int a = 10;
+		int b = a;
+		int x = b;
+
+		Integer x = Integer.valueOf(b);
+	
+	System.out.println(b);
+
+	}
+
+		}
