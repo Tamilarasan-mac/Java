eb562d17846dbfcd8266b16a16bf361b462e22a5
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2025-12-30 10:32:22 +0530

    wrap_array_method.java file added

---
+++ wrap_array_method.java
@@ -0,0 +1,18 @@
+public class wrap_array_method {
+	public static void main(String[] args) {
+		Integer[] arr = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	for(Integer a : arr) {
+
+
+System.out.println("Values :" + a + "Square :" +(a * a));
+
+		}
+
+
+
+	}
+
+
+
+}
