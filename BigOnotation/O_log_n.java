efe19655d1d7382a8cd3b17d7f4d7379675eadf5
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2026-01-03 13:46:24 +0530

    Modified O_log_n filr

--- BigOnotation/O_log_n.java
+++ BigOnotation/O_log_n.java
@@ -4,8 +4,7 @@ public class O_log_n {
     public static int binarySearch(int[] arr, int target) {
         int low = 0;
         int high = arr.length - 1;
-
-        while (low <= high) {
+ while (low <= high) {
             int mid = (low + high) / 2;
 
             if (arr[mid] == target)
@@ -16,6 +15,7 @@ public class O_log_n {
                 high = mid - 1;
         }
         return -1;
+       
     }
 
     public static void main(String[] args) {
