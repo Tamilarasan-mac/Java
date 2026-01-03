beef9478f7d2a71bf9d31d0a97e7a9e489aaa679
Tamilarasan.mac <tamilarasanrajendran@outlook.com>
2026-01-03 14:57:58 +0530

    Modified Binary_search filr

--- Binary_search.java
+++ Binary_search.java
@@ -1,12 +1,41 @@
-import java.util.Arrays;
+public class Binary_search{
+    public static int binarySearch(int[]marks , int find){
+    int low = 0 , high = marks.length-1;
+        while (low <= high){
+            int mid = (low + high)/2;
+        if(marks [mid]==find) 
+            return mid;
+        else if (find < marks[mid])
+            high = mid-1;
 
-public class Binary_search {
-    public static void main(String[] args){
+        else 
+            low = mid+1;
 
-        int[] a = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        int index = Arrays.binarySearch(a,13);
 
-    System.out.println("Index a = " + index );
-    
-    }
+        }
+        return-1;
 }
+public static void main(String[] args){
+        int[] marks ={98,95,85,97,100};
+        int find = 100;
+        int result = binarySearch(marks, find);
+        if (result == -1)
+        System.out.println("Mark not found");
+        else
+        System.out.println("Mark Founded :" + result);
+        
+
+
+}
+
+
+
+
+
+
+
+
+
+}
+
+
